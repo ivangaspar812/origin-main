f6ea77c518abbc17bc20fefb9798a97db638bf3b
Ivan Gaspar <strongest888@yandex.ru>
2023-10-12 13:49:08 +0300

    add tasks 17,18

---
+++ Task17/Program.cs
@@ -0,0 +1,33 @@
+﻿/*
+17. Напишите программу, которая принимает на вход координаты точки (X и Y), причём X ≠ 0 и Y ≠ 0 
+и выдаёт номер четверти плоскости, в которой находится эта точка.
+x=34; y=-30 -> 4 
+x=2; y=4-> 1
+x=-34; y=-30 -> 3
+*/
+
+System.Console.WriteLine("Задай координату точки Х: ");
+int userX = int.Parse(Console.ReadLine());
+System.Console.WriteLine("Задай координату точки Y: ");
+int userY = int.Parse(Console.ReadLine());
+
+if (userX > 0 && userY > 0)
+{
+    System.Console.WriteLine("Точка находится в 1 четверти");
+}
+if (userX < 0 && userY > 0)
+{
+    System.Console.WriteLine("Точка находится в 2 четверти");
+}
+if (userX < 0 && userY < 0)
+{
+    System.Console.WriteLine("Точка находится в 3 четверти");
+}
+if (userX > 0 && userY < 0)
+{
+    System.Console.WriteLine("Точка находится в 4 четверти");
+}
+if (userX == 0 || userY == 0)
+{
+    System.Console.WriteLine("Точка находится на оси координат");
+}
---
+++ Task18/Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+18. Напишите программу, которая по заданному номеру четверти, показывает диапазон 
+возможных координат точек в этой четверти (x и y).
+*/
+
+Console.Clear();
+
+System.Console.WriteLine("Введите номер четверти: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number == 1)
+{
+    System.Console.WriteLine("X > 0 и Y > 0");
+}
+else if (number == 2)
+{
+    System.Console.WriteLine("X < 0 и Y > 0");
+}
+else if (number == 3)
+{
+    System.Console.WriteLine("X < 0 и Y < 0");
+}
+else if (number == 4)
+{
+    System.Console.WriteLine("X > 0 и Y < 0");
+}
+else if (number < 1 || number > 4)
+{
+    System.Console.WriteLine("Нет такой четверти");
+}
